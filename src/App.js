4c11e0a3eff64552351e7a45f9eefde64c98973e
MeirShuvax <124002947+MeirShuvax@users.noreply.github.com>
2024-06-02 22:50:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Play from "./components/Play";
+import NotFound from "./components/NotFound";
+import Leaderboard from './components/Leaderboard';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Header />}>
+                    <Route index element={<Home />} />
+                    <Route path="/play/:data/:gameData" element={<Play />} />
+                    <Route path="/leaderboard" element={<Leaderboard />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
